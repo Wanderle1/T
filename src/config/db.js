159b1b2a9d6309d2f128ae1beeb46b5586f0f579
Wanderlei de Moraes <wanderlei.mora3s@gmail.com>
2024-11-04 12:55:14 -0300

    first

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB conectado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao conectar ao MongoDB:', error);
+    }
+};
+
+
+export default connectDB
